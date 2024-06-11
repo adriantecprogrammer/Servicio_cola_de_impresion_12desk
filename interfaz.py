cc12127338ffa32483d33f88f588ead32eb44c32
adriantecprogrammer <adrian2004mexico.com@gmail.com>
2024-06-10 22:25:41 -0600

    perf: se agrego jun hilo
    se agrego un hilo lo que permite mantener la conexion axctiva con el rpc
    
    close

--- interfaz.py
+++ interfaz.py
@@ -1,18 +1,14 @@
-from email._header_value_parser import ContentTransferEncoding
-from tkinter import *
-
-import mysql.connector
-from dotenv import load_dotenv
+import tkinter as tk
+from tkinter import ttk, messagebox
+import threading
 import os
-import re
+import mysql.connector
+from RPCServer import RPCServer
 from TicketGenerator import TicketGenerator
+from dotenv import load_dotenv
 
-
-# Aqui cargamos los datos del .env
-load_dotenv()
 data = []
-
-
+load_dotenv()
 
 def process_row(row):
     # Acceder a los elementos de la tupla por índice
@@ -38,7 +34,7 @@ def process_row(row):
     latitud = row[21]
     longitud = row[22]
 
-    # Se crea un diccionario para la correcta transformacion de datos
+    # Se crea un diccionario para la correcta transformación de datos
     row_data = {
         "id": id,
         "folio": folio,
@@ -52,7 +48,8 @@ def process_row(row):
         "observaciones": observaciones,
         "fecha_hora_creacion": fecha_hora_creacion,
         "fecha_hora_modificacion": fecha_hora_modificacion,
-        "vendedor_id": vendedor_id,        "zona_id": zona_id,
+        "vendedor_id": vendedor_id,
+        "zona_id": zona_id,
         "direccion": direccion,
         "numero_interior": numero_interior,
         "referencia": referencia,
@@ -65,102 +62,75 @@ def process_row(row):
     return row_data
 
 def obtenerRegistros():
-# Establecer la conexión a la base de datos
- db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    port=int(os.getenv("DB_PORT"))
- )
+    # Establecer la conexión a la base de datos
+    db = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
+    )
 
- # Aqui hacemos la consulta y almacenamos el resultado en una variable, se guardan en tuplas
- cursor = db.cursor()
- cursor.execute("SELECT o.*, d.* FROM Orders o JOIN Direcciones d ON o.address_id = d.iddireccion;")
- resultados = cursor.fetchall()
+    # Aquí hacemos la consulta y almacenamos el resultado en una variable, se guardan en tuplas
+    cursor = db.cursor()
+    cursor.execute("SELECT o.*, d.* FROM Orders o JOIN Direcciones d ON o.address_id = d.iddireccion;")
+    resultados = cursor.fetchall()
 
- # Cerrar la conexión
- cursor.close()
- db.close()
- 
- 
+    # Cerrar la conexión
+    cursor.close()
+    db.close()
 
- # Aqui se almacena la informacion que se transformo de duplas a array
- data.clear()
- for row in resultados:
-    row_data = process_row(row)
-    data.append(row_data)
-
-
-
-ventana = Tk()
-ventana.geometry("700x400")
-ventana.title("12Desk")
-
-# Este marco es para meter la scrollbar
-marco_principal = Frame(ventana)
-marco_principal.pack(fill=BOTH, expand=1)
-
-# El canvas es donde va ir el frame con toda la info
-canvas = Canvas(marco_principal)
-canvas.pack(side=LEFT, fill=BOTH, expand=1)
-
-# Se crea la scrollbar y luego al canvas se le asocia
-scrollbar = Scrollbar(marco_principal, orient=VERTICAL, command=canvas.yview)
-scrollbar.pack(side=RIGHT, fill=Y)
-
-# Configuración de canvas y scrollbar
-canvas.configure(yscrollcommand=scrollbar.set, xscrollcommand=scrollbar.set)
-
-# Se crea un marco y lo ponemos dentro del canvas
-marco_lista = Frame(canvas)
-canvas.create_window((0, 0), window=marco_lista, anchor=NW)
+    # Aquí se almacena la información que se transformó de tuplas a array
+    data.clear()
+    for row in resultados:
+        row_data = process_row(row)
+        data.append(row_data)
 
 def imprimir(datos):
-    
     try:
-        
-     print(f"Imprimiendo {datos["id"]}...")
-     ticket_generator = TicketGenerator(
-     id_pedido =str(datos["id"]),
-     cliente= str(datos["nombre"]),
-     monto=str(datos["total"]),
-     direccion=str(datos["direccion"])+", "+str(datos["numero_interior"])+", "+str(datos["colonia"]),
-     )
-     ticket_generator.generar_ticket("ticket.pdf")
+        print(f"Imprimiendo {datos['id']}...")
+        ticket_generator = TicketGenerator(
+            id_pedido=str(datos["id"]),
+            cliente=str(datos["nombre"]),
+            monto=str(datos["total"]),
+            direccion=str(datos["direccion"]) + ", " + str(datos["numero_interior"]),
+            colonia=str(datos["colonia"])
+            
+        )
+       
+        ticket_generator.generar_ticket("ticket.pdf")
+        messagebox.showinfo("Información", "Pdf creado correctamente")
     except Exception as e:
         print(f"Error: {str(e)}")
-    
+        messagebox.showinfo("Error", "Error al crear pdf")
+
 def actualizar_labels():
-    
     obtenerRegistros()
-   
-    
     for widget in marco_lista.winfo_children():
         widget.destroy()
 
     for i, elemento in enumerate(data):
         # Crear un marco para cada elemento
-        marco_elemento = Frame(marco_lista, bd=1, relief=SOLID)
-        marco_elemento.grid(row=i, column=0, padx=30, pady=10, sticky=W+E)
+        marco_elemento = tk.Frame(marco_lista, bd=1, relief=tk.SOLID)
+        marco_elemento.grid(row=i, column=0, padx=30, pady=10, sticky=tk.W + tk.E)
 
         # Ancho fijo para los labels
         ancho_fijo = 50
 
         # Label para el título
-        label_titulo = Label(marco_elemento, text="ID: " + str(elemento["id"]), font=("Arial", 12, "bold"), width=ancho_fijo, anchor=W)
-        label_titulo.grid(row=0, column=0, padx=10, pady=10, sticky=W)
-        
-         # Label para el título
-        label_titulo = Label(marco_elemento, text="Cliente: " + str(elemento["nombre"]), font=("Arial", 12, "bold"), width=ancho_fijo, anchor=W)
-        label_titulo.grid(row=1, column=0, padx=10, pady=10, sticky=W)
+        label_titulo = tk.Label(marco_elemento, text="ID: " + str(elemento["id"]), font=("Arial", 12, "bold"), width=ancho_fijo, anchor=tk.W)
+        label_titulo.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+        # Label para el cliente
+        label_cliente = tk.Label(marco_elemento, text="Cliente: " + str(elemento["nombre"]), font=("Arial", 12, "bold"), width=ancho_fijo, anchor=tk.W)
+        label_cliente.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
 
         # Label para la descripción
-        label_descripcion = Label(marco_elemento, text=elemento["descripcion"], font=("Arial", 10), width=ancho_fijo, anchor=W)
-        label_descripcion.grid(row=2, column=0, padx=10, pady=10, sticky=W)
+        label_descripcion = tk.Label(marco_elemento, text=elemento["descripcion"], font=("Arial", 10), width=ancho_fijo, anchor=tk.W)
+        label_descripcion.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
 
         # Botón para cada elemento
-        boton_imprimir = Button(marco_elemento, text="Imprimir", command=lambda datos=elemento: imprimir(datos))
+        boton_imprimir = tk.Button(marco_elemento, text="Imprimir", command=lambda datos=elemento: imprimir(datos))
         boton_imprimir.grid(row=0, column=1, rowspan=2, padx=10, pady=10)
         boton_imprimir.config(bg="black", fg="white")
 
@@ -168,11 +138,42 @@ def actualizar_labels():
     marco_lista.update_idletasks()
     canvas.config(scrollregion=canvas.bbox("all"))
 
-# Crear el botón de Refresh en la parte superior derecha
-boton_refresh = Button(ventana, text="Recargar", command=actualizar_labels)
-boton_refresh.pack(side=TOP, anchor=NE, padx=10, pady=10)
+ventana = tk.Tk()
+ventana.geometry("700x400")
+ventana.title("12Desk")
 
-# Llamar a la función para inicializar los labels
+# Este marco es para meter la scrollbar
+marco_principal = tk.Frame(ventana)
+marco_principal.pack(fill=tk.BOTH, expand=1)
+
+# El canvas es donde va ir el frame con toda la info
+canvas = tk.Canvas(marco_principal)
+canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+# Se crea la scrollbar y luego al canvas se le asocia
+scrollbar = tk.Scrollbar(marco_principal, orient=tk.VERTICAL, command=canvas.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+# Configuración de canvas y scrollbar
+canvas.configure(yscrollcommand=scrollbar.set, xscrollcommand=scrollbar.set)
+
+# Se crea un marco y lo ponemos dentro del canvas
+marco_lista = tk.Frame(canvas)
+canvas.create_window((0, 0), window=marco_lista, anchor=tk.NW)
+
+boton_refresh = tk.Button(ventana, text="Recargar", command=actualizar_labels)
+boton_refresh.pack(side=tk.TOP, anchor=tk.NE, padx=10, pady=10)
+
+# Función para iniciar el servidor RPC en un hilo separado
+def iniciar_rpc():
+    rpc_server = RPCServer()
+    rpc_thread = threading.Thread(target=rpc_server.run, daemon=True)
+    rpc_thread.start()
+
+# Iniciar el servidor RPC
+iniciar_rpc()
+
+# Cargar datos iniciales
 actualizar_labels()
 
 ventana.mainloop()
